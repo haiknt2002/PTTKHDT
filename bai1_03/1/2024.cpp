0f1b6471df442a01868ed7739a6263c5ce073777
hai nguyen <haiknt2002@gmail.com>
2024-01-03 10:34:23 +0700

    first commit

---
+++ bai1_03/1/2024.cpp
@@ -0,0 +1,19 @@
+/*
+- Một cô gái (Sury) có:
+    + Một chuồng gồm nhiều ngăn để nuôi các con vật làm cảnh,
+    mỗi ngăn chỉ chứa 1 con, mỗi con đều có tên
+    + Có nhiều loại con vật khác nhau
+- Do số lượng ngày càng nhiều, Sury cần:
+    + Xây dựng 1 chương trình để quản lí các con vật trên
+- Chương trình có nhiệm vụ gì?
+    + Xác định danh tính con vật trong từng ngăn
+    + Thêm con vật vào chuồng, lấy một con vật ra khỏi chuồng
+    + Có thể sửa chữa, mở rộng chương trình một cách dễ dàng
+*/
+/*
+    class Animal:
+        id : int;
+        name : string;
+        style : string;
+        local : int;     //index of Animal in List
+*/
